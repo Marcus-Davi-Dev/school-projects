14a9fe2307e78746b3b8200a4befaa20ebda7e26
Marcus Davi <davimarcus96@gmail.com>
2025-07-16 18:23:44 -0300

    Create device-crasher.js

---
+++ device-crasher/device-crasher.js
@@ -0,0 +1,18 @@
+const recursionCrashBtn = document.querySelector("#recursion");
+
+const timedCrashBtn = document.querySelector("#timed-btn");
+const timedCrashInput = document.querySelector("#timed-input");
+
+recursionCrashBtn.addEventListener("click", function(){
+  // make recursion until reach the limit
+  // dont last too much
+  function a(){a();}
+  a();
+});
+
+timedCrashBtn.addEventListener("click", function(){
+  const milisseconds = parseInt(timedCrashInput) * 1000;
+  let stop = false;
+  setTimeout(()=>{stop = true}, milisseconds);
+  while(!stop){}
+});
