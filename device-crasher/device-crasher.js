073903865434be232901006dfb24a15f76387879
Marcus Davi <davimarcus96@gmail.com>
2025-07-16 18:48:27 -0300

    make recursion crash calculate a fibonacci number

--- device-crasher/device-crasher.js
+++ device-crasher/device-crasher.js
@@ -4,10 +4,15 @@ const timedCrashBtn = document.querySelector("#timed-btn");
 const timedCrashInput = document.querySelector("#timed-input");
 
 recursionCrashBtn.addEventListener("click", function(){
-  // make recursion until reach the limit
-  // dont last too much
-  function a(){a();}
-  a();
+  // calculate the 46 number of the fibonacci sequence.
+  // may last like a minute or more.
+  function fib(n){
+    if(n === 1 || n === 0){
+      return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
+  a(46);
 });
 
 timedCrashBtn.addEventListener("click", function(){
